cbac62ce1b94fa117d6e2293f0b9a27d8d3f7d38
PRANAV K DILEEP <91619846+pranavkdileep@users.noreply.github.com>
2024-05-12 06:53:44 +0000

    Update link to Whatsapp channel in Mainhero component

--- cybernotes/components/mainhero.tsx
+++ cybernotes/components/mainhero.tsx
@@ -74,9 +74,9 @@ export function Mainhero() {
                 <div className="flex flex-col gap-2 min-[400px]:flex-row">
                   <Link
                     className="inline-flex h-10 items-center justify-center rounded-md bg-gray-900 px-8 text-sm font-medium text-gray-50 shadow transition-colors hover:bg-gray-900/90 focus-visible:outline-none focus-visible:ring-1 focus-visible:ring-gray-950 disabled:pointer-events-none disabled:opacity-50 dark:bg-gray-50 dark:text-gray-900 dark:hover:bg-gray-50/90 dark:focus-visible:ring-gray-300"
-                    href="#"
+                    href="https://whatsapp.com/channel/0029VaaAhdE8PgsBEeFUwo2W"
                   >
-                    Telegram Channel
+                    Whatsapp Channel
                   </Link>
                   </div>
               </div>
