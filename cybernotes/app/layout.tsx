fb9434c79c0e30f4b0f17026d18ccdca12dcf9b4
PRANAV K DILEEP <91619846+pranavkdileep@users.noreply.github.com>
2024-11-29 18:52:21 +0000

    google anaysis

--- cybernotes/app/layout.tsx
+++ cybernotes/app/layout.tsx
@@ -2,6 +2,8 @@ import type { Metadata } from "next";
 import { Inter } from "next/font/google";
 import "./globals.css";
 import { Analytics } from "@vercel/analytics/react"
+import { GoogleAnalytics } from '@next/third-parties/google'
+
 
 const inter = Inter({ subsets: ["latin"] });
 
@@ -20,6 +22,7 @@ export default function RootLayout({
     <html lang="en">
       <body className={inter.className}>{children}
       <Analytics />
+      <GoogleAnalytics gaId="G-3E9CVT98PP" />
       </body>
     </html>
     
