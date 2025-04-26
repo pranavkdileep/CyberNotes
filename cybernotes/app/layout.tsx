f6029a6421b86b430daaca997baccb1a6626fe96
PRANAV K DILEEP <91619846+pranavkdileep@users.noreply.github.com>
2025-04-26 10:12:39 +0000

    ad

--- cybernotes/app/layout.tsx
+++ cybernotes/app/layout.tsx
@@ -1,15 +1,15 @@
 import type { Metadata } from "next";
 import { Inter } from "next/font/google";
 import "./globals.css";
-import { Analytics } from "@vercel/analytics/react"
-import { GoogleAnalytics } from '@next/third-parties/google'
-
+import { Analytics } from "@vercel/analytics/react";
+import { GoogleAnalytics } from "@next/third-parties/google";
 
 const inter = Inter({ subsets: ["latin"] });
 
 export const metadata: Metadata = {
   title: "KTU Cyber Security Notes",
-  description: "A platform to access study materials and syllabi for students of b tech computer science engineering (syber security) in KTU university",
+  description:
+    "A platform to access study materials and syllabi for students of b tech computer science engineering (syber security) in KTU university",
 };
 
 export default function RootLayout({
@@ -18,13 +18,30 @@ export default function RootLayout({
   children: React.ReactNode;
 }>) {
   return (
-    
     <html lang="en">
-      <body className={inter.className}>{children}
-      <Analytics />
-      <GoogleAnalytics gaId="G-3E9CVT98PP" />
+      <head>
+        <script
+          type="module"
+          src="https://securepubads.shareus.io/scripts/tag/js/gpt.js"
+        ></script>
+      </head>
+      <body className={inter.className}>
+        {children}
+        <Analytics />
+        <GoogleAnalytics gaId="G-3E9CVT98PP" />
+        <div id="shareus-admanager-8188567387-rqqS8xm7Or">
+          <script
+            dangerouslySetInnerHTML={{
+              __html: `
+                window._shareustag = window._shareustag || {cmd:[]};
+                _shareustag.cmd.push(function() {
+                  var slot1 = _shareustag.defineSlot('/shareus-admanager/8188567387/rqqS8xm7Or/Bodyad', ["Responsive"], 'shareus-admanager-8188567387-rqqS8xm7Or');
+                });
+              `,
+            }}
+          ></script>
+        </div>
       </body>
     </html>
-    
   );
 }
